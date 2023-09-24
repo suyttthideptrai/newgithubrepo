79f58493e838ed178f77a87c7caa7b23afff2b67
NguyenDuc <duc61551@gmail.com>
2023-09-24 17:51:46 +0700

    phan footer va header

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,5 +1,99 @@
-<footer>
-    <hr>
-    this is the footer part
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link rel="stylesheet" href="./test.css"> -->
+    <!-- <link rel="stylesheet" href="./footer.css"> -->
+</head>
+
+<body>
+    <div class="container bg-dark text-white">
+        <footer class="py-4 footer-items">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <h5>LOGO</h5>
+                    <!-- <h5 style="border-bottom: solid 1px white;">Logo</h5> -->
+                    <ul class="nav flex-column mt-2">
+                        <li class="nav-item mb-2 ms-4">Address: 285 Doi Can</li>
+                        <li class="nav-item ms-4">Email: Moonlight@gmail.com</li>
+
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Moonlight</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Home</a></li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">About Us</a>
+                        </li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Blog</a></li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Festival</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Festival1</a>
+                        </li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Festival2</a>
+                        </li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Festival3</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Country</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Country1</a>
+                        </li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Country2</a>
+                        </li>
+                        <li class="nav-item mb-1"><a class="nav-link p-0" href="" style="color:inherit">Country3</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Follow Us</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-1">
+                            <a class="nav-link p-0 me-5 " target="_blank" href="https://twitter.com/home" style="color:inherit">
+                                <i class="fa-brands fa-twitter fa-bounce"></i>
+                                Twitter
+                            </a>
+                        </li>
+                        <li class="nav-item mb-1">
+                            <a class="nav-link p-0 me-5" target="_blank" href="https://www.instagram.com/?hl=en" style="color:inherit">
+                                <i class="fa-brands fa-instagram fa-bounce"></i>
+                                Instagram                                
+                            </a>
+                        </li>
+                        <li class="nav-item mb-1">
+                            <a class="nav-link p-0 me-5" target="_blank" href="https://www.facebook.com/" style="color:inherit">
+                                <i class="fa-brands fa-facebook fa-bounce"></i>
+                                Facebook                          
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="my-1 py-3 border-top">
+                <p>© 2023 Moonlight, Inc. All rights reserved</p>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+</body>
+
 </html>
--- layouts/header.php
+++ layouts/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -36,4 +36,203 @@
     }
     ?>
     <hr>
-</header>
+</header> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#search-button').click(function () {
+                $('#search-txt').toggle(200);
+            });
+        });
+    </script>
+
+</head>
+
+<body>
+<style>
+    * {
+    box-sizing: border-box;
+    color: black;
+}
+body{
+    background-color: black;
+}
+
+.container{
+    justify-content: space-around ;
+    align-items: center;
+    /* height: 30px; */
+}
+
+.navbar-collapse{
+    flex-grow: 0;
+}
+
+#search-box{
+    background-color:transparent;
+    border-radius: 30px;
+    margin-right: 10px;
+}
+
+#search-txt{
+    outline:none;
+    border-radius: 20px;;
+    padding: 5px 10px;
+}
+
+#search-button{
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    padding:10px;
+}
+
+.test{
+    width: 100px;
+}
+</style>
+    <div class="navbar navbar-expand-lg bg-white">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href="home.html" class="navbar-brand">
+                <h2>Logo</h2>
+            </a>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-1 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a href="home.html" class="nav-link active">Home</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu menu1">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+            <div id="#ultis " class="d-flex align-items-center">
+
+                <form id="search-box" method="get">
+                    <button type="button" id="search-button">
+                        <i class="fa-solid fa-magnifying-glass" style="color:black"></i>
+                    </button>
+                    <input placeholder="Search" type="text" id="search-txt" style="display:none">
+                </form>
+
+                <div class="dropdown">
+
+                    <a class="user" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <!-- <li><a class="dropdown-item" href="#">Sign Up</a></li> -->
+                        <li><a class="dropdown-item" href="#">Sign In</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
